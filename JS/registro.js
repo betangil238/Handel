c6334b17579c23c3058db6087cc8eece6610974a
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-09-26 21:05:15 -0500

    verificacion contrasena, correo y nombre

--- JS/registro.js
+++ JS/registro.js
@@ -1,9 +1,23 @@
+//Verificación del input name
+const nombre = document.getElementById("name");
+
+nombre.addEventListener("input", function () {
+    //Verificar si el valor del input contiene números
+    
+    if (/\d/.test(nombre.value)) {
+        mostrarAlertaRechazo("No se admiten numeros en el nombre");
+        //Si contiene números, mostrar un mensaje de error
+        //alert("No se permiten números en el texto.") ;
+        nombre.value = nombre.value.replace(/\d/g, ""); // Eliminar los números
+    } 
+});
+
+
 // Inicializamos una variable que recibirá los datos del formulario de registro
 const signupForm = document.querySelector('#signupForm')
 
 // Inicializamos una función que nos permitirá envíar los datos del formulario
-signupForm.addEventListener('submit', (e) => 
-{
+signupForm.addEventListener('submit', (e) => {
     //  Nos ayuda a evitar que se envíe datos sin dar click al botón
     e.preventDefault()
     // Inicializamos una variable que almacenará el valor ingresado en el campo del nombre
@@ -12,22 +26,8 @@ signupForm.addEventListener('submit', (e) =>
     const email = document.querySelector('#email').value
     // Inicializamos una variable que almacenará el valor ingresado en el campo de password
     const password = document.querySelector('#password').value
-    // Inicializamos una variable que recibirá los datos de los usuarios registrados, si no hay datos almacenados, se creará un arreglo vacío
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    // Inicializamosuna variable que buscará si el email que ingresamos es exactamente igual al que registramos
-    // El método .find se encarga de buscar un elemento en la lista de objetos
-    const isUserRegistered = Users.find(user => user.email === email)
-
-
-    
-
-
-
-    //Si el email que desee registrar, ya existe, nos dirá que ya se encuentra registrado, sino me permitirá ingresar sin problema al sistema
-    if(isUserRegistered) {
-        mostrarAlertaRechazo();
-        // return alert('El usuario ya esta registrado!')
-    }else{
+    if(validarpassword() && validarUsuario() && validarCorreo() ){
+        const Users = JSON.parse(localStorage.getItem('users')) || []
         // Aquí especificamos que nos agregue los datos a la lista
         Users.push({name: name, email: email, password: password})
 
@@ -36,16 +36,82 @@ signupForm.addEventListener('submit', (e) =>
 
         // Aquí especificamos de que si el registro fue correcto, entonces nos aparecerá un msj de alerta de que fue exitoso
         // alert('Registro Exitoso!')
+        
         mostrarAlerta();
-
+        signupForm.reset();
         // Si el registro fue exitoso, nos redigirá al login luego de 3 segundos
         setTimeout(() => {
             window.location.href = 'login.html';
         }, 2500);
     }
-
-   
 })
+
+// VERIFICACION CONTRASEÑAS REGISTRO EN HANDEL
+function validarpassword(){
+//Id de contraseña de HTML
+var p1 = document.getElementById("password").value;
+var p2 = document.getElementById("password1").value;
+
+//Evita que queden espacios en blanco
+var espacios = false;
+var cont = 0;
+
+    while (!espacios && (cont < p1.length)) {
+        if (p1.charAt(cont) == " ")
+            espacios = true;
+            cont++;
+    }
+
+//Evita que quede un campo de contraseña vacío
+    if (espacios) {
+        mostrarAlertaRechazo("La contraseña no puede contener espacios en blanco")
+        return false;
+    }
+
+    if (p1.length == 0 || p2.length == 0) {
+        mostrarAlertaRechazo("La contraseña no puede contener espacios en blanco");
+        // alert("Los campos de la password no pueden quedar vacios");
+        return false;
+    }
+
+  //Validar que las contraseñas coincidan 
+    if (p1 != p2) {
+        mostrarAlertaRechazo("Las contraseñas deben de coincidir");
+        return false;
+    } else {
+        return true; 
+    }
+}
+
+// FUNCION VALIDAR CORREO
+function validarCorreo(){
+    const email = document.querySelector('#email');
+    const expresionRegularCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    //Validar el correo electrónico con la expresión regular
+    if (!expresionRegularCorreo.test(email.value)) {
+        mostrarAlertaRechazo("Ingrese una dirección de correo en este formato: example@email.com");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// FUNCION QUE VALIDA SI EL USUARIO YA ESTA CREADO
+function validarUsuario(){
+     // Inicializamos una variable que almacenará el valor ingresado en el campo del email
+    const email = document.querySelector('#email').value
+    // Inicializamos una variable que recibirá los datos de los usuarios registrados, si no hay datos almacenados, se creará un arreglo vacío
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    // Inicializamosuna variable que buscará si el email que ingresamos es exactamente igual al que registramos
+    // El método .find se encarga de buscar un elemento en la lista de objetos
+     const isUserRegistered = Users.find(user => user.email === email)
+     if(isUserRegistered) {
+        mostrarAlertaRechazo(`El usuario ya esta registrado!`);
+        return false;
+    }else{
+        return true;
+    }
+}
 
 function mostrarAlerta() {
     Swal.fire({
@@ -64,10 +130,10 @@ function mostrarAlerta() {
 //funcion para mostrar el mensaje durante un tiempo en ms
 
 // funcion que muestra una alerta de error
-function mostrarAlertaRechazo() {
+function mostrarAlertaRechazo(mensaje) {
     Swal.fire({
         title: 'Error',
-        text: `El usuario ya esta registrado!`,
+        text: mensaje,
         icon: 'error',
         confirmButtonText: 'Cerrar',
         customClass: {
@@ -78,3 +144,4 @@ function mostrarAlertaRechazo() {
         }
     });
 }
+
