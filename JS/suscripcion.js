d44a2956e35d9c99fdf4d8baaa754f7e8725886a
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-10-02 10:49:47 -0500

    documentacion suscripcion y eliminacion tarjetas fijas

--- JS/suscripcion.js
+++ JS/suscripcion.js
@@ -1,5 +1,4 @@
 import KEYS from "../JS/keys.js"
-/* inicio encabezado------------------------------------------------------------*/ 
 
 // <!------------------------------------------------------ INICIO SECCION MAIN ------------------------------------------->
 const user= JSON.parse(localStorage.getItem('login_success')) || false
@@ -17,14 +16,17 @@ const FormatoMoneda = num => `${num.slice(0,-5)}`
 
 
 let products, price;
+// Realiza dos solicitudes de API en paralelo y procesa los resultados
 Promise.all([
     fetch("https://api.stripe.com/v1/products",options),
     fetch("https://api.stripe.com/v1/prices",options)
 ])
 .then(responses =>Promise.all(responses.map(res => res.json())))
 .then(json => {
+    // Almacena los datos de productos y precios en las variables 'products' y 'price'
     products = json[0].data;
     price=json[1].data;
+     // Itera sobre los precios y crea tarjetas de producto para mostrar en la página
     price.forEach(el => {
         let productDta = products.filter(product => product.id === el.product);
         tarjeta.querySelector(".buy").setAttribute("data-price",el.id);
@@ -38,15 +40,19 @@ Promise.all([
     ecomerce.appendChild(fragment)
 })
 .catch(error => {
+      // Manejo de errores en caso de fallas en las solicitudes
     let message = error.statuText || "ocurrio un error en la peticion";
     ecomerce.innerHTML = `Error: ${error.status}: ${message}`
 })
+
+// Agrega un manejador de eventos click al documento
 d.addEventListener("click", e=>{
     if (e.target.matches(".incrementShop, .incrementIcon")) {
         if (e.target.classList.contains('bx') || e.target.classList.contains('bxs')) {
             color(e.target); // Llama a la función color() y pasa el ícono como argumento
         }
     } else if (e.target.matches(".E-comerce *") ) {
+         // Realiza una acción relacionada con la compra del producto
         let priceId = e.target.parentElement.getAttribute("data-price");
         Stripe(KEYS.public).redirectToCheckout({
             lineItems: [{
