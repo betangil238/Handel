bf18c9500cdca8505e470637b5848fe6aca39be4
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-10-01 21:02:20 -0500

    Pasarela de pagos

--- JS/suscripcion.js
+++ JS/suscripcion.js
@@ -1,3 +1,4 @@
+import KEYS from "../JS/keys.js"
 /* inicio encabezado------------------------------------------------------------*/ 
 let arreglo=[];
 let carritoVisible = false;
@@ -52,24 +53,6 @@ function openCar() {
 
 
 // <!------------------------------------------------------ INICIO SECCION MAIN ------------------------------------------->
-
-
-
-$(document).ready(function() {
-    $(".bx").click(function() {
-        // Obtener la clase actual del icono
-        var claseActual = $(this).attr("class");
-
-        // Verificar la clase actual y cambiarla
-        if (claseActual === 'bx bx-heart') {
-            $(this).removeClass('bx bx-heart').addClass('bx bxs-heart');
-            this.style.color="red";
-        } else {
-            $(this).removeClass('bx bxs-heart').addClass('bx bx-heart');
-            this.style.color="white";
-        }
-    });
-});
 
 function agregarProducto(producto){
     const cantidadProduct=document.getElementById("cantidad");
@@ -170,3 +153,81 @@ function indice(producto){
         }
     }
 }
+
+// ******* CONEXION CON EL MARKET PLACE *****************************************
+
+const d =document;
+const ecomerce=d.getElementById("E-comerce");
+const tarjeta = d.getElementById("tarjeta").content;
+const fragment=d.createDocumentFragment();
+const options = { headers : {Authorization: `Bearer ${KEYS.secret}`}}
+const FormatoMoneda = num => `${num.slice(0,-5)}`
+
+
+let products, price;
+Promise.all([
+    fetch("https://api.stripe.com/v1/products",options),
+    fetch("https://api.stripe.com/v1/prices",options)
+])
+.then(responses =>Promise.all(responses.map(res => res.json())))
+.then(json => {
+    products = json[0].data;
+    price=json[1].data;
+    price.forEach(el => {
+        let productDta = products.filter(product => product.id === el.product);
+        tarjeta.querySelector(".buy").setAttribute("data-price",el.id);
+        tarjeta.querySelector("img").src=productDta[0].images[0];
+        tarjeta.querySelector("img").alt=productDta[0].name;
+        tarjeta.querySelector(".descriptionecomerce").innerHTML=`${productDta[0].name}`;
+        tarjeta.querySelector(".priceecomerce").innerHTML=`${FormatoMoneda(el.unit_amount_decimal)}${"K"}`;
+        let clone =d.importNode(tarjeta,true);
+        fragment.appendChild(clone);
+    });
+    ecomerce.appendChild(fragment)
+})
+.catch(error => {
+    let message = error.statuText || "ocurrio un error en la peticion";
+    ecomerce.innerHTML = `Error: ${error.status}: ${message}`
+})
+
+
+
+d.addEventListener("click", e=>{
+    if (e.target.matches(".E-comerce *") ) {
+        let priceId = e.target.parentElement.getAttribute("data-price");
+        Stripe(KEYS.public).redirectToCheckout({
+            lineItems: [{
+                price: priceId,
+                quantity: 1
+            }],
+            mode:"payment",
+            successUrl:"http://127.0.01:5501/success.html",
+            cancelUrl:"http://127.0.01:5501/cancel.html"
+        })
+        .then(res => {
+            if (res.error){
+                ecomerce.insertAdjacentElement("afterend", res.error.message)
+            }
+        })
+    }
+})
+
+// funcion que cambia de color el corazon al darle click
+$(document).ready(function() {
+    $(".bx").click(function() {
+        alert("click")
+        // Obtener la clase actual del icono
+        var claseActual = $(this).attr("class");
+
+        // Verificar la clase actual y cambiarla
+        if (claseActual === 'bx bx-heart') {
+            $(this).removeClass('bx bx-heart').addClass('bx bxs-heart');
+            this.style.color="red";
+        } else {
+            $(this).removeClass('bx bxs-heart').addClass('bx bx-heart');
+            this.style.color="white";
+        }
+    });
+});
+
+
