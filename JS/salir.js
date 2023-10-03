1ecaffbe4438599daf58b8c6c162c90c22be5253
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-10-03 17:33:48 -0500

    conexion datos sofi, Juli, Dani y links

---
+++ JS/salir.js
@@ -0,0 +1,23 @@
+const logout=document.getElementById("logout")
+logout.addEventListener('click',()=>{
+    mostrarAlerta();
+    localStorage.removeItem('login_success')
+    setTimeout(() => {
+        window.location.href='login.html';
+    }, 2500);
+})
+
+function mostrarAlerta() {
+    Swal.fire({
+        title: 'Muchas gracias',
+        text: 'Hasta pronto',
+        icon: 'success', // Puedes cambiar el icono (success, error, warning, info, etc.)
+        confirmButtonText: 'Aceptar', // Texto del boton
+        customClass: {
+            container: 'mi-alerta',
+            title: 'mi-titulo',
+            content: 'mi-contenido',
+            confirmButton: 'mi-boton'
+        }
+    });
+}
