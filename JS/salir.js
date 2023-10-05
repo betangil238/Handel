9d62e148b90c62df878b7e075b1f8ada4d0e18b4
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-10-05 10:33:24 -0500

    vista previa actualizada

--- JS/salir.js
+++ JS/salir.js
@@ -1,3 +1,8 @@
+const user= JSON.parse(localStorage.getItem('login_success')) || false
+if (!user) {
+    window.location.href="login.html"  
+}
+
 const logout=document.getElementById("logout")
 logout.addEventListener('click',()=>{
     mostrarAlerta();
