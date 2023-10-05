eea15b244cab4664f5c393d8e34127f941680859
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-10-05 17:35:42 -0500

    validaciones publicacion

---
+++ JS/infoArchivo.js
@@ -0,0 +1,30 @@
+const siguiente=document.getElementById("siguiente");
+siguiente.addEventListener("click",function(){
+    const titulo=document.getElementById("titulo").value;
+    const descripcion=document.getElementById("descripcion").value;
+    if(titulo=="" && descripcion==""){
+        mostrarAlertaRechazo("Diligencie todos los campos");
+    }else if(titulo==""){
+        mostrarAlertaRechazo("Diligencie el campo de titulo");
+    }else if(descripcion==""){
+        mostrarAlertaRechazo("Diligencie el campo de descripcion");
+    }
+    else{
+        window.location.href= 'vistaPrevia.html';
+    }
+})
+
+function mostrarAlertaRechazo(mensaje) {
+    Swal.fire({
+        title: 'Error',
+        text: mensaje,
+        icon: 'error',
+        confirmButtonText: 'Cerrar',
+        customClass: {
+            container: 'mi-alerta-error',
+            title: 'mi-titulo-error',
+            content: 'mi-contenido-error',
+            confirmButton: 'mi-boton-error'
+        }
+    });
+}
--- JS/subirarchivo.js
+++ JS/subirarchivo.js
@@ -2,11 +2,11 @@ const user= JSON.parse(localStorage.getItem('login_success')) || false
 if (!user) {
     window.location.href="login.html"  
 }
-
+var contador=0;
 function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
-  
+      contador++;
       reader.onload = function(e) {
         var imageUploadWrap = document.querySelector('.image-upload-wrap');
         var fileUploadImage = document.querySelector('.file-upload-image');
@@ -26,6 +26,7 @@ function readURL(input) {
   }
   
   function removeUpload() {
+    contador--;
     var fileUploadInput = document.querySelector('.file-upload-input');
     var fileUploadContent = document.querySelector('.file-upload-content');
     var imageUploadWrap = document.querySelector('.image-upload-wrap');
@@ -44,3 +45,26 @@ function readURL(input) {
     imageUploadWrap.classList.remove('image-dropping');
   });
 
+function validar(){
+  const fileUpload = document.querySelector('.file-upload-input');
+  if(fileUpload.files.length===0 ||contador<=0 ){
+    mostrarAlertaRechazo("Cargue una imagen")
+  }else{
+    window.location.href = 'informacionArchivo.html'
+  }
+}
+
+  function mostrarAlertaRechazo(mensaje) {
+    Swal.fire({
+        title: 'Error',
+        text: mensaje,
+        icon: 'error',
+        confirmButtonText: 'Cerrar',
+        customClass: {
+            container: 'mi-alerta-error',
+            title: 'mi-titulo-error',
+            content: 'mi-contenido-error',
+            confirmButton: 'mi-boton-error'
+        }
+    });
+}
