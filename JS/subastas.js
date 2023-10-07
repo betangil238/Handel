16b366ed5aafaf5ccd28dffffc5dc00cea243382
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-10-06 19:07:36 -0500

    seccion subastas responsive

--- JS/subastas.js
+++ JS/subastas.js
@@ -9,7 +9,8 @@ const productos=[{imagen:"Img/subastas/1.png",id:"965458",likes:"320",vistas:"42
 {imagen:"Img/subastas/6.png",id:"123",likes:"2",vistas:"678",categoria:"Belleza",visibilidad:"privado",etiquetas:"usado, antiguo"},
 {imagen:"Img/subastas/7.png",id:"124",likes:"23",vistas:"546",categoria:"Juguetes",visibilidad:"publico",etiquetas:"nuevo, personalizado"},
 {imagen:"Img/subastas/8.png",id:"125",likes:"77",vistas:"323",categoria:"Ropa",visibilidad:"publico",etiquetas:""},
-{imagen:"Img/subastas/9.png",id:"126",likes:"88",vistas:"325",categoria:"Bebes",visibilidad:"publico",etiquetas:"usado"}
+{imagen:"Img/subastas/9.png",id:"126",likes:"88",vistas:"325",categoria:"Bebes",visibilidad:"publico",etiquetas:"usado"},
+{imagen:"Img/subastas/12.png",id:"126",likes:"88",vistas:"325",categoria:"Bebes",visibilidad:"publico",etiquetas:"usado"}
 ]
 
 const empaquetador=document.querySelector(".ContenidoSubastas");
