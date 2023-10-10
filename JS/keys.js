63c201b97dd69f268f12ce0f6eadb7614dd90473
Sofia <squimbayc@unal.edu.co>
2023-10-10 13:34:38 -0500

    Se añade js pruebas1

--- JS/keys.js
+++
@@ -1,4 +0,0 @@
-export default {
-    public: "pk_test_51NwXpQHYVb9dCPBpPedu8IvqMdhnd07ezVZcC6Lh5lxj6q4d7soGVijCBQ7fJs7l3UFmhXtKR48qEX44Cwl8gdRl00toS03u7n",
-    secret:"sk_test_51NwXpQHYVb9dCPBplbf3g5A3KHerZxZuKPanF93ll7xpKbtO5qkcUrMA1djPtf1A7N3djFCflCrrBV2ILjrQczMx00wC8ABhdJ"
-}
--- JS/newPassword.js
+++
@@ -1,4 +0,0 @@
-const verificacionPrevia= JSON.parse(localStorage.getItem('USUARIO')) || false
-if (!verificacionPrevia) {
-    window.location.href="forgotPassword.html"  
-}
