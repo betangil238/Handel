f335dc54177ba9464e3eaa7f4059dd17870f7365
lvalentinal23 <lvalentinal23@hotmail.com>
2023-10-12 22:27:48 -0500

    Funcionalidad enviar mensajes en Chat

--- JS/mensajes.js
+++ JS/mensajes.js
@@ -1,1 +1,38 @@
+const inputMessage = document.querySelector(".inputMessage");
+
+const containerInput = document.querySelector(".containerAllMessages");
+
+const sendButton = document.querySelector(".sendButtonClick");
+
+/*Especificaciones para botón de enviar*/
+sendButton.addEventListener("click", function(){
+    const message = inputMessage.value;
+
+    if(inputMessage.value != "") {
+        containerInput.innerHTML+=`<div class="greyMessages">
+        <div class="messageSent">
+            <p>${message}</p>
+        </div></div>`
+    
+        inputMessage.value=""; 
+    }
+})
+
+/*Input enter*/
+inputMessage.addEventListener("keydown", function(e){
+
+    if(e.key=== 'Enter' && inputMessage.value != ""){
+        const message = inputMessage.value;
+
+        containerInput.innerHTML+=`<div class="greyMessages">
+        <div class="messageSent">
+            <p>${message}</p>
+        </div></div>`
+    
+        inputMessage.value="";
+    }
+
+})
+
+
 
