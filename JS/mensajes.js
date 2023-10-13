150c576995a1c148c95941d7d8d8a168fc4faacb
lvalentinal23 <lvalentinal23@hotmail.com>
2023-10-12 19:33:32 -0500

    Mensajes nada nuevo

--- JS/mensajes.js
+++ JS/mensajes.js
@@ -1,7 +1,1 @@
-/*Ocultar container 1 al dar click en un chat individual - Cel view*/
-var chat = false;
 
-$("button").click(ocultarContainer()) {
-    var  btn =  chat === false ?  true  :  false ;
-    btn === true ? $("#containerHiden").hide(): $("showContainer").show();
-};
